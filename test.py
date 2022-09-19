8365172f5f8cabe255e5e02f3213fad7257caa02
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2022-09-19 08:37:18 +0400

    test

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+import lxml
+
+url = "https://yandex.ru/images/search?text=zebra"
+r = requests.get(url);
+r.text
+soup = BeautifulSoup(r.text, "lxml")
+zebra = soup.findAll('img', class_='serp-item__thumb justifier__thumb')
+print(len(zebra))
