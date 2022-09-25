a41d257f1562822260e2982959ad3b01a82ef060
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2022-09-25 16:58:05 +0400

    final

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from utilyes import run
 from time import sleep
 if __name__ == '__main__':
     run('zebra')
-    #sleep(10)
-    #print('\nfirst part end\n')
-    #run('zebra')
+    sleep(10)
+    print('\nfirst part end\n')
+    run('bay horse')
     
