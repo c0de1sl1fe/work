90c3865fdbb00c119a5d2a1f6aa9816a94204665
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2022-09-25 14:50:46 +0400

    a lot of work (it's wrong to commit this way)

--- test.py
+++ test.py
@@ -1,21 +1,5 @@
-
-import os
-import imp
-from bs4 import BeautifulSoup
-import requests
-import lxml
-from selenium.webdriver import Chrome
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait as wait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from time import sleep
+from utilyes import run
 
 
-def CreateUrl(request, link = 'https://yandex.ru/images/search?text='):
-    return link + request.replace(' ', '%20')
-
-    
-print("test")
-str = "t t"
-print(CreateUrl(str))
+if __name__ == '__main__':
+    run('zebra')
--- utilyes.py
+++ utilyes.py
@@ -2,18 +2,111 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 import os
-import test
-#n = 1000
+from time import sleep
+import shutil
 
-#soup = BeautifulSoup(r.text, "lxml")
-#zebra = soup.findAll('img', class_='serp-item__thumb justifier__thumb')
 
-def CreateUrl(srt):
-    str.replace(' ', '%20')
-    url = 'https://yandex.ru/images/search?text=' +str+ 'zebra&p=1'
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'lxml')
-    soup.find('img', class_='serp-item__thumb justifier__thumb').get('src')
+def create_url(request):
+    data = []
+    for n in range(1, 6):
+        print ("Parsing ", n+1 , " page")
+        request.replace(' ', '%20')
+        url = f'https://yandex.ru/images/search?text={request}&p={n}'
+        r = requests.get(url)
+        sleep(3)
+        soup = BeautifulSoup(r.text, 'lxml')
+        tmp = soup.find_all('img', class_='serp-item__thumb justifier__thumb')
+        for img in tmp:
+            test = 'https:' + img.get('src')
+            data.append(test)
+    return data
+
+
+
+""" 
+def download(img_url, img_name, img_path):
+    src = requests.get(img_url)
+    with open(f'{img_path}/{img_name}.jpg', 'ab') as file:
+        file.write(src)
+    file.close()     """
+
+
+
+
+""" def get_image_url(item):
+    for page in range(1, 2):
+        url = f'https://yandex.ru/images/search?p={page}&text={item}'
+        src = requests.get(url)
+        soup = BeautifulSoup(src.text, "lxml")
+        all_images = soup.find_all("a", class_="serp-item__link")
+        for image in all_images:
+            url_img = "https:" + image.find("img", class_="serp-item__thumb").get("src")
+            yield url_img """
+
+""" def create_dir(src):
+    if not os.path.isdir('dataset'):
+        os.mkdir('dataset')
+    os.chdir('dataset')
+    if not os.path.exists(src):
+        os.mkdir(src)
+    oldpwd = os.getcwd()
+ """
+def create_dir(src):
+    if not os.path.isdir('dataset'):
+        os.mkdir('dataset')
+    if not os.path.exists(f'dataset/{src}'):
+        os.mkdir(f'dataset/{src}')
+
+
+
+
+""" def download_image(url_image, name, item):
+    src = requests.get(url_image, stream=True)
+    with open(f'{item}/{name}.jpg', 'ab') as file:
+        for iteration in src.iter_content(1024**2):
+            file.write(iteration)
+    file.close()
+ """
+
+def download_img(img_url, img_name, img_path):
+    response = requests.get(img_url)
+    file = open(f"dataset/{img_path}/{img_name}.jpg", "wb")
+    file.write(response.content)
+    file.close()
+
+
+def run(class_name):
+    create_dir(class_name)
+    test = create_url(class_name)
+    number = 0
+    for item in test:
+        download_img(item, str(number).zfill(4), class_name)
+        number += 1
+
+""" def run():
+    create_dir('rose')
+    create_dir('tulip')
+
+    number = 0
+    for url_of_item in get_image_url("rose"):
+        number += 1
+        download_image(url_of_item, str(number).zfill(4), "rose")
+        print(1)
+        sleep(1)
+
+    number = 0
+    for url_of_item in get_image_url("tulip"):
+        number += 1
+        download_image(url_of_item, str(number).zfill(4), "tulip")
+        print(2)
+        sleep(1)
+    os.chdir(oldpwd)
+ """
+
+print(1)
+
+
+
 
 #def getAllBlock(url):
    # r = requests.get(url)
