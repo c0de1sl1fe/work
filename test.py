99f8cd08d9e81709b8fe30da9e4e57391b464b16
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2022-10-04 23:04:39 +0400

    111

--- test.py
+++ test.py
@@ -3,27 +3,148 @@ from time import sleep
 import cv2
 import numpy as np
 import os
+import os.path
 
 def cmp(image_1: cv2.Mat, image_2: cv2.Mat) -> bool:
-  return image_1 == image_2
+    dsize = (400, 400)
+    test_1 = cv2.resize(image_1, dsize)
+    test_2 = cv2.resize(image_2, dsize)
+
+    return test_1 == test_2
 def fix_dataset(path_dir):
-    image_11 = cv2.imread("dataset/zebra/0001.jpg")
-    image_12 = cv2.imread("dataset/zebra/0002.jpg")
 
-    i, j = 0, 0
+    if os.path.isdir(f'dataset/{path_dir}'):
+        i, j = 0, 0
 
-    while():
-        while():
-            if(np.all(cmp(image_11, image_12) == True)):
-                os.remove()
-            j+=1
+        path_1 = f'dataset/{path_dir}/{str(i).zfill(4)}.jpg'
+        path_2 = f'dataset/{path_dir}/{str(j).zfill(4)}.jpg'
         
-        i+=1 
+        while(os.path.exists(path_1)):
+            path_1 = f'dataset/{path_dir}/{str(i).zfill(4)}.jpg'
+            j = 0
+            while(os.path.exists(path_2)):
+                path_2 = f'dataset/{path_dir}/{str(j).zfill(4)}.jpg'
+                img_1 = cv2.imread(path_1)
+                img_2 = cv2.imread(path_2)
+                    
+                if np.all(cmp(img_1, img_2) == True) and i != j:
+                    print("DUblicate: ", path_2)
+                    os.remove(path_2)
+                j+=1
+                path_2 = f'dataset/{path_dir}/{str(j).zfill(4)}.jpg'
+            
+            i+=1
+            path_1 = f'dataset/{path_dir}/{str(i).zfill(4)}.jpg'
+    else:
+        print("Error of exist dir")
+    print(i, " ", j)
+
+
+def remove_duplicate(path_dir):
+    path = f'dataset/{path_dir}'
+    flag = False
+    exit = False
+    while( not exit):
+        flag = False
+        names = os.listdir(path)
+        inner_names = os.listdir(path)
+        for filename1 in names:
+            if(flag):
+                break
+            for filename2 in inner_names:
+                if(flag):
+                    break
+                img_1 = cv2.imread(f'{path}/{filename1}')
+                img_2 = cv2.imread(f'{path}/{filename2}')
+                if np.all(cmp(img_1, img_2) == True) and filename1!=filename2:
+                    print("DUblicate: ", filename1," and ", filename2)
+                    os.remove(f'{path}/{filename2}')
+                    flag = True
+        if not flag:
+            exit = True
+
+
+def fix_dataset(path_dir):
+
+    if os.path.isdir(f'dataset/{path_dir}'):
+        i, j = 0, 0
+
+        path_1 = f'dataset/{path_dir}/{str(i).zfill(4)}.jpg'
+        path_2 = f'dataset/{path_dir}/{str(j).zfill(4)}.jpg'
+        
+        while(os.path.exists(path_1)):
+            path_1 = f'dataset/{path_dir}/{str(i).zfill(4)}.jpg'
+            j = 0
+            while(os.path.exists(path_2)):
+                path_2 = f'dataset/{path_dir}/{str(j).zfill(4)}.jpg'
+                img_1 = cv2.imread(path_1)
+                img_2 = cv2.imread(path_2)
+                    
+                if np.all(cmp(img_1, img_2) == True) and i != j:
+                    print("DUblicate: ", path_2)
+                    os.remove(path_2)
+                j+=1
+                path_2 = f'dataset/{path_dir}/{str(j).zfill(4)}.jpg'
+            
+            i+=1
+            path_1 = f'dataset/{path_dir}/{str(i).zfill(4)}.jpg'
+    else:
+        print("Error of exist dir")
+    print(i, " ", j)
+
+
+def remove_duplicate1(path_dir):
+    path = f'dataset/{path_dir}'
+    flag = False
+    exit = False
+    while( not exit):
+        flag = False
+        names = os.listdir(path)
+        inner_names = os.listdir(path)
+        filename1 = os.listdir(path)
+        while not flag:
+            while not flag:
+                img_1 = cv2.imread(f'{path}/{filename1}')
+                img_2 = cv2.imread(f'{path}/{filename2}')
+                if np.all(cmp(img_1, img_2) == True) and filename1!=filename2:
+                    print("DUblicate: ", filename1," and ", filename2)
+                    os.remove(f'{path}/{filename2}')
+                    flag = True
+        
+        for filename1 in names:
+            if(flag):
+                break
+            for filename2 in inner_names:
+                if(flag):
+                    break
+                
+        if not flag:
+            exit = True
+
+
+
+def remove_duplicate2(path_dir):
+    path = f'dataset/{path_dir}'
+    names = os.listdir(path)
+    for filename1 in names:
+        for filename2 in names:
+            img_1 = cv2.imread(f'{path}/{filename1}')
+            img_2 = cv2.imread(f'{path}/{filename2}')
+            if np.all(cmp(img_1, img_2) == True) and filename1!=filename2:
+                print("DUblicate: ", filename1," and ", filename2)
+                os.remove(f'{path}/{filename2}')
+                names.remove(filename2)
+
+
+
+
 
 if __name__ == '__main__':
-    image_11 = cv2.imread("dataset/zebra/0001.jpg")
-    image_12 = cv2.imread("dataset/zebra/0002.jpg")
-
-    print()
+    remove_duplicate2('bay horse')
+    # path_dir = 'tiger'
+    # path = f'dataset/{path_dir}'
+    # filename1 = os.listdir(path)
+    # print(filename1)
+    #fix_dataset('bay horse')
     #print(cmp(image_11, image_12))
     
