1375804451ebbac31d6afad4122dec64868e73d2
_Yury_ <90544915+c0de1sl1fe@users.noreply.github.com>
2022-10-05 19:07:31 +0400

    final fix, already to pass

--- utilyes.py
+++ utilyes.py
@@ -1,8 +1,58 @@
-from bs4 import BeautifulSoup
-import requests
-import lxml
 import os
 from time import sleep
+
+from bs4 import BeautifulSoup
+import requests
+import cv2
+import numpy as np
+import os.path
+
+
+def cmp(image_1: cv2.Mat, image_2: cv2.Mat) -> bool:
+    dsize = (400, 400)
+    test_1 = cv2.resize(image_1, dsize)
+    test_2 = cv2.resize(image_2, dsize)
+    return test_1 == test_2
+
+
+def remove_duplicate_slow(path_dir):
+    path = f'dataset/{path_dir}'
+    del_arr = []
+    names = os.listdir(path)
+    for filename1 in names:
+        del_arr = []
+        for filename2 in names:
+            img_1 = cv2.imread(f'{path}/{filename1}')
+            img_2 = cv2.imread(f'{path}/{filename2}')
+            if np.all(cmp(img_1, img_2) == True) and filename1 != filename2:
+                print("DUblicate: ", filename1, " and ", filename2)
+                os.remove(f'{path}/{filename2}')
+                del_arr.append(filename2)
+        print(filename1)
+        for i in del_arr:
+            print('to delete  ', i)
+            names.remove(i)
+
+
+def remove_duplicate_fast(path_dir):
+    if os.path.isdir(f'dataset/{path_dir}'):
+        path = f'dataset/{path_dir}'
+        names = os.listdir(path)
+        i, j = 0, 0
+        length_names = len(names)
+        while i < length_names:
+            j = i
+            while j < length_names:
+                img_1 = cv2.imread(f'{path}/{names[i]}')
+                img_2 = cv2.imread(f'{path}/{names[j]}')
+                if np.all(cmp(img_1, img_2) == True) and i != j:
+                    print("DUblicate: ", names[i], " and ", names[j])
+                    os.remove(f'{path}/{names[j]}')
+                j += 1
+            print(names[i])
+            names = os.listdir(path)
+            length_names = len(names)
+            i += 1
 
 
 def create_url(request):
